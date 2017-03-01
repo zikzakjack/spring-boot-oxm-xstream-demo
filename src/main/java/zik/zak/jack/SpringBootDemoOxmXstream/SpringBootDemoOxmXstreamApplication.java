36858d0e5af708c8159e46a62ce66bffc830da33
balasuks <balasuks@W01BCJ28.UBSPROD.MSAD.UBS.NET>
2017-03-01 10:33:17 -0500

    Initial Spring Boot Project

---
+++ src/main/java/zik/zak/jack/SpringBootDemoOxmXstream/SpringBootDemoOxmXstreamApplication.java
@@ -0,0 +1,12 @@
+package zik.zak.jack.SpringBootDemoOxmXstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOxmXstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoOxmXstreamApplication.class, args);
+	}
+}
---
+++ src/test/java/zik/zak/jack/SpringBootDemoOxmXstream/SpringBootDemoOxmXstreamApplicationTests.java
@@ -0,0 +1,16 @@
+package zik.zak.jack.SpringBootDemoOxmXstream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoOxmXstreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
